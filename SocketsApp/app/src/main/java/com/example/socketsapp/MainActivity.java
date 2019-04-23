bddeacf26e94584ffbedf1921ff79010bcccc099
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-04-23 16:06:52 +0530

    Mood activity is created

--- SocketsApp/app/src/main/java/com/example/socketsapp/MainActivity.java
+++ SocketsApp/app/src/main/java/com/example/socketsapp/MainActivity.java
@@ -14,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.moods);
         e1 = (EditText)findViewById(R.id.editText);
 
     }
