f7555786cc36c8daa313f064db521aac0d78110c
Gaurav Gandhi <gauravgo4friends@gmail.com>
2019-04-23 12:31:59 +0530

    something doe

--- SocketsApp/app/src/main/java/com/example/socketsapp/MainActivity.java
+++ SocketsApp/app/src/main/java/com/example/socketsapp/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.socketsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText e1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        e1 = (EditText)findViewById(R.id.editText);
+
+    }
+
+    public void send(View view) {
+        MessageSender messageSender = new MessageSender();
+        messageSender.execute(e1.getText().toString());
     }
 }
